eb671fbf4dd8959891f98b51a0c2868383628238
Math7usFilipe <mxtheus829melo@gmail.com>
2025-02-07 10:32:04 -0300

    classe VeiculoController criada

---
+++ src/main/java/fcamara/desafio/estacionamento/Controller/VeiculoController.java
@@ -0,0 +1,74 @@
+package fcamara.desafio.estacionamento.Controller;
+
+import fcamara.desafio.estacionamento.Services.VeiculoService;
+import fcamara.desafio.estacionamento.entities.Veiculo;
+import fcamara.desafio.estacionamento.entities.enums.TipoVeiculo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping ("/veiculos")
+public class VeiculoController {
+
+    @Autowired
+    private VeiculoService veiculoService;
+
+    @PostMapping
+    public ResponseEntity<Veiculo> criarVeiculo(@RequestBody Veiculo veiculo) {
+        Veiculo novoVeiculo = veiculoService.criarVeiculo(veiculo);
+        return new ResponseEntity<>(novoVeiculo, HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Veiculo>> listarVeiculo() {
+        List<Veiculo> veiculos = veiculoService.listarVeiculo();
+        return new ResponseEntity<>(veiculos, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Veiculo> obterVeiculoPorId(@PathVariable Long id) {
+        return veiculoService.buscarVeiculoPorId(id)
+                .map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @PutMapping ("/{id}")
+    public  ResponseEntity<Veiculo> atualizarVeiculo(@PathVariable Long id, @RequestBody Veiculo veiculo) {
+        try {
+            Veiculo veiculoAtualizado = veiculoService.atualizarVeiculo(id, veiculo);
+            return new ResponseEntity<>(veiculoAtualizado, HttpStatus.OK);
+        } catch (RuntimeException e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PostMapping ("/entrada/{id}")
+    public ResponseEntity<String> registraEntrada(@PathVariable Long id) {
+        try {
+            String msg = veiculoService.registrarEntrada(id);
+            return ResponseEntity.ok(msg);
+        } catch (RuntimeException e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PostMapping ("/saida/{id}")
+    public ResponseEntity<String> registraSaida(@PathVariable Long id) {
+        try {
+            String msg = veiculoService.registraSainda(id);
+            return ResponseEntity.ok(msg);
+        } catch (RuntimeException e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @GetMapping ("/tipo/{tipo}")
+    public ResponseEntity<List<Veiculo>> buscarPorTipo(@PathVariable TipoVeiculo tipo) {
+        List<Veiculo> veiculos = veiculoService.buscarVeiculosPorId(tipo);
+        return new ResponseEntity<>(veiculos, HttpStatus.OK);
+    }
+}
