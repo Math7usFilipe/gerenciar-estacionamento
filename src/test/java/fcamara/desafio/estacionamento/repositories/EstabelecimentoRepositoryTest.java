687c7a31974c59b961dedae948bf3703a2ebcf3a
Math7usFilipe <mxtheus829melo@gmail.com>
2025-02-10 11:47:23 -0300

    criado EstabelecimentoRepositoryTest

---
+++ src/test/java/fcamara/desafio/estacionamento/repositories/EstabelecimentoRepositoryTest.java
@@ -0,0 +1,15 @@
+package fcamara.desafio.estacionamento.repositories;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+class EstabelecimentoRepositoryTest {
+
+    @Test
+    void findByCnpj() {
+
+    }
+}
